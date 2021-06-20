17382d7f6e112e6f38b4b01e61d2c6090dcc99d1
jacek.lange <jacek.lange@qed.pl>
2021-06-20 13:47:39 +0200

    Add basic detail view

--- src/App.js
+++ src/App.js
@@ -1,7 +1,20 @@
-import {ChakraProvider, Input, Link} from "@chakra-ui/react"
+import {AspectRatio, ChakraProvider, Input, Link, Spinner} from "@chakra-ui/react"
 import './App.css';
 import {Stack, Button} from "@chakra-ui/react"
 import {useState} from "react"
+
+import { useDisclosure } from "@chakra-ui/react"
+
+import {
+  Modal,
+  ModalOverlay,
+  ModalContent,
+  ModalHeader,
+  ModalFooter,
+  ModalBody,
+  ModalCloseButton,
+} from "@chakra-ui/react"
+
 import {
   Table,
   Thead,
@@ -12,31 +25,65 @@ import {
     Td
 } from "@chakra-ui/react"
 
+function Detail({ result }) {
+  const { isOpen, onOpen, onClose } = useDisclosure()
+  return (
+    <>
+      <Button onClick={onOpen} colorScheme="blue" >View</Button>
+
+      <Modal isOpen={isOpen} onClose={onClose}>
+        <ModalOverlay />
+        <ModalContent>
+          <ModalHeader>{result.trackName}</ModalHeader>
+          <ModalCloseButton />
+          <ModalBody>
+            <AspectRatio maxW="580px" ratio={1}>
+                <iframe title="listen" src={result.previewUrl} allowFullScreen />
+            </AspectRatio>
+          </ModalBody>
+          <ModalFooter>
+            <Button colorScheme="blue" mr={3} onClick={onClose}>
+              Close
+            </Button>
+            <Button variant="ghost">Secondary Action</Button>
+          </ModalFooter>
+        </ModalContent>
+      </Modal>
+    </>
+  )
+}
+
 function Itunes() {
-    const [searchTerm, setSarchTerm] = useState("");
+    const [searchTerm, setSearchTerm] = useState("");
     const [results, setResults] = useState([
     ])
+    const [showSpinner, setShowSpinner] = useState(false);
+
     return (
         <Stack direction="column">
             <Stack direction="row">
                 <Input value={searchTerm} onChange={(event) => {
-                    setSarchTerm(event.target.value);
+                    setSearchTerm(event.target.value);
                 }}></Input>
                 <Button colorScheme="blue" onClick={async () => {
+                    setShowSpinner(true);
                     const result = await fetch(
                         `https://itunes.apple.com/search?term=${encodeURIComponent(searchTerm)}&entity=musicVideo`
                     );
                     const data = await result.json();
+                    setShowSpinner(false);
                     console.log(data.results);
                     setResults(data.results);
                 }}>Search</Button>
             </Stack>
+            {showSpinner && <Spinner />}
             if (results.length) {
             <Table variant="simple">
                 <Thead>
                     <Tr>
                         <Th>Artist</Th>
                         <Th>Track</Th>
+                        <Th></Th>
                         <Th></Th>
                     </Tr>
                 </Thead>
@@ -46,7 +93,11 @@ function Itunes() {
                             <Tr>
                                 <Td>{result.artistName}</Td>
                                 <Td><Link href={result.trackViewUrl} isExternal>{result.trackName}</Link></Td>
-                                <Td><Image borderRadius="full" objectFit="cover" width="100px" height="100px" src={result.artworkUrl100}/></Td>
+                                <Td>
+                                    <Image borderRadius="full" objectFit="cover" width="100px" height="100px" src={result.artworkUrl100}/></Td>
+                                <Td>
+                                    <Detail result={result} />
+                                </Td>
                             </Tr>
                         ))
                     }
