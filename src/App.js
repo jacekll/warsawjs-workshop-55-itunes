8e35b5e5d53d62b4a5a4ad4d5a706bcea4eb4f7d
jacek.lange <jacek.lange@qed.pl>
2021-06-20 12:04:02 +0200

    Add first version of the app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,71 @@
-import logo from './logo.svg';
+import {ChakraProvider, Input, ListItem, UnorderedList} from "@chakra-ui/react"
 import './App.css';
+import {Badge, Stack, Button} from "@chakra-ui/react"
+import {useState} from "react"
+
+function Itunes() {
+    const [searchTerm, setSarchTerm] = useState("");
+    const [results, setResults] = useState([
+        {
+            name: "WarsawJS",
+        },
+        {
+            name: "React",
+        }
+    ])
+    return (
+        <Stack direction="column">
+            <Stack direction="row">
+                <Input value={searchTerm} onChange={(event) => {
+                    setSarchTerm(event.target.value);
+                }}></Input>
+                <Button colorScheme="blue" onClick={async () => {
+                    const result = await fetch(
+                        `https://itunes.apple.com/search?term=${encodeURIComponent(searchTerm)}&entity=musicVideo`
+                    );
+                    const data = await result.json();
+                    console.log(data);
+                    setResults(data.results);
+                }}>Search</Button>
+            </Stack>
+            <UnorderedList>
+                {
+                    results.map((result) => (
+                        <ListItem>{result.trackName} by {result.artistName}</ListItem>
+                    ))
+                }
+                <ListItem>bla</ListItem>
+                <ListItem>bla 2</ListItem>
+            </UnorderedList>
+        </Stack>
+    )
+}
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [count, setCount] = useState(5);
+    return (
+        <ChakraProvider>
+            <div className="App">
+                <header className="App-header">
+                    <Stack direction="column">
+                        <Itunes></Itunes>
+
+                        <Stack direction="row">
+                            <Badge>{count}</Badge>
+                            <Button colorScheme="green" onClick={() => {
+                                setCount(count + 1);
+                            }}>+</Button>
+                            <Button colorScheme="red" onClick={() => {
+                                setCount(count - 1);
+                            }}>+</Button>
+                        </Stack>
+                    </Stack>
+                </header>
+
+
+            </div>
+        </ChakraProvider>
+    );
 }
 
 export default App;
